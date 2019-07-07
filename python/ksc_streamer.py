50d8d3d56555fbfbe5c4df3d46eb68f172277569
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2019-07-07 10:18:34 +0430

    refine kafka spark cassandra code

---
+++ python/ksc_streamer.py
@@ -0,0 +1,55 @@
+from _datetime import datetime
+import random
+import string
+
+from pyspark import SparkConf, SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+import pyspark_cassandra
+from pyspark_cassandra import streaming
+# from pyspark_cassandra.streaming import saveToCassandra
+
+
+# spark-submit
+# --master local[2] --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.3,anguenot:pyspark-cassandra:0.10.1
+
+def random_string(string_length=10):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(string_length))
+
+
+# or for master: "spark://MASTER:7077"
+conf = SparkConf() \
+    .setAppName("Streaming test") \
+    .set("spark.cassandra.connection.host", "127.0.0.1")
+sc = SparkContext(conf=conf)
+ssc = StreamingContext(sc, 5)
+
+topic = "test"
+kafka_stream = KafkaUtils.createStream(ssc,
+                                       # Zookeeper quorum (hostname:port,hostname:port,..)
+                                       "localhost:9092",
+                                       # group ID
+                                       "topic",
+                                       # topics with their corresponding partition
+                                       {topic: 1})
+
+raw = kafka_stream.flatMap(lambda x: [x])
+time_now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+# clean = raw.map(lambda xs: xs[1].split(","))
+
+# Match cassandra table fields with dictionary keys
+# this reads input of format: x[partition, timestamp]
+my_row = raw.map(lambda x: {
+    "testid": str(x),
+    "date": random_string(),
+    "time1": time_now,
+    "time2": time_now
+})
+
+# save to cassandra
+my_row.saveToCassandra("tutorialspoint", "test1")
+
+ssc.start()  # Start the computation
+ssc.awaitTermination()  # Wait for the computation to terminate
